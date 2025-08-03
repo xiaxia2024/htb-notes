6dda586683f2273309fc39bce236b0a66ad541f5
xiaxia2024 <155163892+xiaxia2024@users.noreply.github.com>
2025-08-04 02:48:47 +0800

    Update ImageMagick.c
    1

--- retired/Titanic/ImageMagick.c
+++ retired/Titanic/ImageMagick.c
@@ -1,1 +1,16 @@
+#!/bin/bash
+timeout="10000"
 
+scrot /tmp/screen.png
+[[ -f ~/.config/i3/lock.png ]] &&
+convert /tmp/screen.png -paint l -swirl 360 ~/.config/i3/lock.png -gravity center -composite -matte /temp/screen.png
+
+mocp -P
+mpc pause
+
+i3lock -e -f -c 000000 -i /tmp/screen.png
+
+while [[ $(pgrep -x i3lock) ]];do
+  [[ $timeout -lt $(xssstate -i) ]] && xset dpms force off
+  sleep 5
+done
